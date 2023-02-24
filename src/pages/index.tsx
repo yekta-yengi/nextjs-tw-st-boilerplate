5afdfb9163c163a0bbdf0ba4a5596796861bf435
Yekta Yurtseven <yurtseven.yektayengi@gmail.com>
2023-02-24 18:30:54 +0300

    Project structure created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,9 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
+          <span className='bg-red-500 font-bold'>
+            Get started by seven&nbsp;
+          </span>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
